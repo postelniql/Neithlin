8cf00775a34f99d1b4be2808467661d8f6a2d4d1
Vlad-Cosmin Postelnicu <vlad.postelnicu@thehutgroup.com>
2023-07-09 19:06:59 +0100

    chore: add ws server and wire it to a minimal UI

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,18 +1,48 @@
+import express from "express";
+import { Server } from "ws";
+
 import { followLinks } from "./links";
-import { downloadUrl } from "./page";
 
-const testUrl = new URL("https://example.com");
-const TEST_DEPTH = 3;
+const app = express();
+app.use(express.static("public"));
 
-(async (url) => {
-  try {
-    const initialLink = url.href;
+const wss = new Server({ noServer: true });
 
-    const scraper = followLinks(initialLink, TEST_DEPTH);
-    for await (let link of scraper) {
-      console.log(`Yielding link: ${link.url} at depth ${link.depth}`);
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log(`Received: ${message}`);
+  });
+
+  const testUrl = new URL("https://example.com");
+  const TEST_DEPTH = 4;
+
+  (async (url) => {
+    try {
+      const initialLink = url.href;
+
+      const scraper = followLinks(initialLink, TEST_DEPTH);
+      for await (let link of scraper) {
+        const now = new Date();
+        const timeString = `${now.getHours().toString().padStart(2, "0")}:${now
+          .getMinutes()
+          .toString()
+          .padStart(2, "0")}:${now.getSeconds().toString().padStart(2, "0")}`;
+        ws.send(
+          `Yielding link: ${link.url} at depth ${link.depth} at ${timeString}`
+        );
+      }
+    } catch (error) {
+      console.error(`Failed to get links of page with error: ${error}`);
     }
-  } catch (error) {
-    console.error(`Failed to get links of page with error: ${error}`);
-  }
-})(testUrl);
+  })(testUrl);
+});
+
+const server = app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
+
+server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit("connection", ws, request);
+  });
+});
