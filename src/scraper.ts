c57d13e55864e452ec524976b983e6203f4c5865
Vlad-Cosmin Postelnicu <vlad.postelnicu@thehutgroup.com>
2023-07-09 12:47:50 +0100

    chore: refactor entry point to IIFE

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,7 +1,20 @@
 import { getLinks } from "./links";
 import { downloadUrl } from "./page";
 
-const testUrl = new URL("https://example.com");
-downloadUrl(testUrl)
-  .then((data) => (data ? console.log(getLinks(data)) : console.log("no data")))
-  .catch((err) => console.error(err));
+const testUrl = new URL("https://wikipedia.com");
+
+(async (url) => {
+  try {
+    const data = await downloadUrl(url);
+
+    if (!data) {
+      console.log("no links found");
+      return null;
+    }
+
+    const links = getLinks(data);
+    console.log(links);
+  } catch (error) {
+    console.error(`Failed to get links of page with error: ${error}`);
+  }
+})(testUrl);
